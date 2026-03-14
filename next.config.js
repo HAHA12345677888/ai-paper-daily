d6e5c4cc831652f3d1c003bdc7578043df5b0345
HAHA12345677888 <134194194+HAHA12345677888@users.noreply.github.com>
2026-03-14 11:08:28 +0800

    Update next.config.js for image settings and output
    Removed remote image patterns and added export output.

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'substackcdn.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'substack-post-media.s3.amazonaws.com',
-      },
-    ],
+    unoptimized: true,
   },
 }
 
